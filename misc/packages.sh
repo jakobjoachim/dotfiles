4edbd348f5fd390cf900d3cb252cdc7ee11ec715
Jakob Joachim <mail@jakobjoachim.com>
2019-04-03 21:57:13 +0200

    kde icon theme added

--- misc/packages.sh
+++ misc/packages.sh
@@ -1,2 +1,16 @@
-# sudo pacman -Syu kate dolphin termite zsh ark dolphin-plugins gwenview kgpg kleopatra kmix ksystemlog kteatime okular print-manager spectacle chromium openssh ttf-hack ttf-dejavu noto-fonts ttf-roboto ttf-liberation dina-font otf-overpass
+/bin/bash
+
+sudo pacman -Syu kate git dolphin termite zsh ark dolphin-plugins gwenview kgpg kleopatra kmix ksystemlog kteatime okular print-manager spectacle chromium openssh ttf-hack ttf-dejavu noto-fonts ttf-roboto ttf-liberation dina-font otf-overpass code adapta-kde adapta-kde adapta-gtk-theme kvantum-qt5 kvantum-theme-adapta
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mkdir -p ~/doc/desktop ~/dev ~/img ~/tmp
+cd ~/tmp
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay enpass 
+yay spotify
+
+
