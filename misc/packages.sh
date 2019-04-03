fbc808046d2e335e3555bb6700e9806536aea106
Jakob Joachim <mail@jakobjoachim.com>
2019-04-03 20:07:04 +0200

    stow format created

---
+++ misc/packages.sh
@@ -0,0 +1,2 @@
+# sudo pacman -Syu kate dolphin termite zsh ark dolphin-plugins gwenview kgpg kleopatra kmix ksystemlog kteatime okular print-manager spectacle chromium openssh ttf-hack ttf-dejavu noto-fonts ttf-roboto ttf-liberation dina-font otf-overpass
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
